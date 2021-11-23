c10bf2d14ae0e7b28b8ac7b938ab81b924c764db
Hossein Araghi <hoseinaraghi84@gmail.com>
2021-11-23 18:25:01 +0330

    Tests

--- src/test/kotlin/ApplicationTests.kt
+++ src/test/kotlin/ApplicationTests.kt
@@ -1,11 +1,14 @@
+import math.add
+import math.calc
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
 
 class ApplicationTests
 {
     @Test
     fun test1()
     {
-        assertEquals(2, 2)
+        println(calc("PI"))
+        println(add(2, 2))
+        println(add(2.4, 2.6))
     }
 }
